3dfe6874769fa38ca62d5a5c354b1d40b6beae6d
ternsip <ternsip@gmail.com>
2016-04-10 21:19:57 +0300

    Fixed internal modname

--- 1.7.10/src/main/java/com/ternsip/stabilizermod/Stabilizermod.java
+++ 1.7.10/src/main/java/com/ternsip/stabilizermod/Stabilizermod.java
@@ -23,7 +23,7 @@ public class Stabilizermod {
 
     public static final String MODID = "stabilizermod";
     public static final String MODNAME = "StabilizerMod";
-    public static final String VERSION = "2.1";
+    public static final String VERSION = "2.2";
     public static final String AUTHOR = "Ternsip";
     public static final String MCVERSION = "1.7.*";
 
